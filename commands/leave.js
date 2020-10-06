70a7026c615f1cf1699c1ed5c9655b6b9a2b3cb9
crash_ip21 <crash.ip21@gmail.com>
2020-10-06 19:09:51 +0500

    updated dependencies

--- commands/leave.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'leave',
-  description: 'leave',
-  permissions: ['MANAGE_CHANNELS', 'MANAGE_ROLES'],
-  execute(message) {
-    message.guild.me.voice.channel.leave();
-  },
-};
